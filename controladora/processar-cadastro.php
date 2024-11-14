00f32809da8747689e7a87ab17cacac91e879783
kirihugo <141354362+kirihugo@users.noreply.github.com>
2024-11-14 19:34:58 -0300

    Update processar-cadastro.php

--- controladora/processar-cadastro.php
+++ controladora/processar-cadastro.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); // Criptografa a senha
 
-    $query = "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)";
+    $query = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("sss", $nome, $email, $senha);
 

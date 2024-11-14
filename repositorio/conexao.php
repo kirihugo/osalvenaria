a225dc74226fafb2bd08edebebf696e0b5afeb58
kirihugo <141354362+kirihugo@users.noreply.github.com>
2024-11-14 18:56:10 -0300

    Add files via upload

---
+++ repositorio/conexao.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+
+//username      Local: root
+//senha         Local: ""
+//databasename  LOCAL: osalvenaria
+
+//username      HOMOLOGAÇÃO: hostdeprojetos_osalvenaria
+//senha         HOMOLOGAÇÃO: ifspgru@2022
+//databasename  HOMOLOGAÇÃO: hostdeprojetos_osalvenaria;
+
+$username = "hostdeprojetos_osalvenaria";
+$password = "ifspgru@2022";
+$databasename = "hostdeprojetos_osalvenaria";
+
+$username = "root";
+$password = "";
+$databasename = "hostdeprojetos_osalvenaria";
+//criação da conexão
+$conn = new mysqli($servername, $username, $password, $databasename);
+
+// verificando a conexão
+if (!$conn){
+    //die("conexão falhou".mysqli_connect_error());
+    echo "não foi possível conectar ao banco de dados";
+}
+
+
+?>

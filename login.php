9f89da767c2d00e2018edda3313fd140867bda5d
kirihugo <141354362+kirihugo@users.noreply.github.com>
2024-11-19 18:27:00 -0300

    Update login.php

--- login.php
+++ login.php
@@ -91,5 +91,22 @@
             });
         });
     </script>
+     <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+  <div id="preloader">
+    <div class="line"></div>
+  </div>
+
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  
+  <script src="assets/js/main.js"></script>
+
 </body>
 </html>

a57e635ec8e1a3762a1ab5d92dd02c38894398d7
kirihugo <141354362+kirihugo@users.noreply.github.com>
2024-11-19 19:20:42 -0300

    Update processar-cadastro.php

--- controladora/processar-cadastro.php
+++ controladora/processar-cadastro.php
@@ -4,18 +4,17 @@ require "../repositorio/conexao.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
-    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); // Criptografa a senha
+    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);  
+
 
     $query = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("sss", $nome, $email, $senha);
 
-
-
     if ($stmt->execute()) {
-        echo 'success'; // Retorna 'success' após cadastro bem-sucedido
+        echo "Cadastro realizado com sucesso!";
     } else {
-        echo 'error'; // Retorna 'error' se o cadastro falhar
+        echo "Ocorreu um erro ao realizar o cadastro: " . $stmt->error;
     }
 }
 ?>

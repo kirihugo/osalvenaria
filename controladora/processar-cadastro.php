9c23a6d922fabd09f8b9e2646f21d0c341342ba3
kirihugo <141354362+kirihugo@users.noreply.github.com>
2024-11-23 23:02:05 -0300

    Add files via upload

---
+++ controladora/processar-cadastro.php
@@ -0,0 +1,20 @@
+<?php
+require "../app/config.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);  
+
+
+    $query = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $nome, $email, $senha);
+
+    if ($stmt->execute()) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Ocorreu um erro ao realizar o cadastro: " . $stmt->error;
+    }
+}
+?>

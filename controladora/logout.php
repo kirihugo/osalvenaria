9663563d249649b3e4e8100736161f20cea28b51
kirihugo <141354362+kirihugo@users.noreply.github.com>
2024-11-23 23:01:10 -0300

    Delete controladora directory

--- controladora/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Inicia a sessão
-session_unset(); // Remove todas as variáveis de sessão
-session_destroy(); // Destrói a sessão
-header("Location: login.php"); // Redireciona para a página de login
-exit(); // Garante que o código abaixo não seja executado
-?>
